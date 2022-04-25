7ed89dacf86e0f16bc029db701a8cfb4da333e86
Stebenwolf <benjamin@the-org.eu>
2022-04-25 19:55:01 +0200

    test

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter basename="/kasa/build"> 
+      <BrowserRouter basename="/kasa/build" > 
       
       <Header />
       <Suspense fallback={<div>Loading...</div>}>
